ba5c248c79b09e7d7b2c1fedafa819204b3919a6
Christerpher Hunter <djhunter67@gmail.com>
2022-05-22 17:03:37 -0400

    partially defined ltr_grade function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+//Let's convert a number grade to a letter grade, using if and elif statements and comparisons. First have the user enter a number representing the grade (0-100). Then convert the number grade to a letter grade.
+
+use std::io;
+
+fn ltr_grade(user_input: u8) -> String {
+    let grade_equiv: String;
+    if user_input > 89 || user_input < 101 {
+        grade_equiv = "A".to_string();
+        grade_equiv
+    } else if user_input > 79 || user_input < 90 {
+        grade_equiv = "B".to_string();
+        grade_equiv
+    } else {
+        grade_equiv = "input not recognized".to_string();
+        grade_equiv
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Prompt user for number grade
+    println!("Please enter your number grade: ");
+
+    // Mutable initialized string variable; contents = ""
+    let mut num_grade: String = String::new();
+
+    io::stdin()
+        .read_line(&mut num_grade)
+        .expect("Failed to read line");
+    loop {
+        let num_grade: u8 = match num_grade.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Letter Grade: {}", ltr_grade(num_grade));
+    }
 }
